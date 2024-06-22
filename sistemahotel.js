ab84c9297da37a38c5c86358f60892f3257074d9
Marcelle Castro <131879933+marczlle@users.noreply.github.com>
2024-06-22 16:28:30 -0300

    Update sistemahotel.js

--- sistemahotel.js
+++ sistemahotel.js
@@ -1,21 +1,21 @@
 // Sistema de hotel
-var readlineSync = require('readline-sync');
+let readlineSync = require('readline-sync');
 
-var tipoQuarto = readlineSync.question("Qual tipo de quarto? ")
-var quantidadeDias = readlineSync.question("Quantas diárias? ")
+let tipoQuarto = readlineSync.question("Qual tipo de quarto? ")
+let quantidadeDias = readlineSync.question("Quantas diárias? ")
 
 if (tipoQuarto == "standard") {
-    var calculoStandart = quantidadeDias * 150
+    let calculoStandart = quantidadeDias * 150
     console.log(`Tipo de quarto: Standard
         Quantidade de diárias: ${quantidadeDias}
         Valor total da estadia: R$ ${calculoStandart.toFixed(2)}`);
 } else if (tipoQuarto == "luxo") {
-    var calculoLuxo = quantidadeDias * 300
+    let calculoLuxo = quantidadeDias * 300
     console.log(`Tipo de quarto: Luxo
         Quantidade de diárias: ${quantidadeDias}
         Valor total da estadia: R$ ${calculoLuxo.toFixed(2)}`);
 } else if (tipoQuarto == "suite") {
-    var calculoSuite = quantidadeDias * 500
+    let calculoSuite = quantidadeDias * 500
     console.log(`Tipo de quarto: Suíte
         Quantidade de diárias: ${quantidadeDias}
         Valor total da estadia: R$ ${calculoSuite.toFixed(2)}`);
