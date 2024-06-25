a2642cd0928637f68244a1b60ed058cea31fa807
Marcelle Castro <131879933+marczlle@users.noreply.github.com>
2024-06-25 13:55:49 -0300

    Update sistemahotel.js

--- sistemahotel.js
+++ sistemahotel.js
@@ -1,22 +1,32 @@
-// Sistema de hotel
-let readlineSync = require('readline-sync');
+//HOTEL
+var readlineSync = require('readline-sync');
 
-let tipoQuarto = readlineSync.question("Qual tipo de quarto? ")
-let quantidadeDias = readlineSync.question("Quantas diárias? ")
+function inicioDoPrograma() {
 
-if (tipoQuarto == "standard") {
-    let calculoStandart = quantidadeDias * 150
-    console.log(`Tipo de quarto: Standard
-        Quantidade de diárias: ${quantidadeDias}
-        Valor total da estadia: R$ ${calculoStandart.toFixed(2)}`);
-} else if (tipoQuarto == "luxo") {
-    let calculoLuxo = quantidadeDias * 300
-    console.log(`Tipo de quarto: Luxo
-        Quantidade de diárias: ${quantidadeDias}
-        Valor total da estadia: R$ ${calculoLuxo.toFixed(2)}`);
-} else if (tipoQuarto == "suite") {
-    let calculoSuite = quantidadeDias * 500
-    console.log(`Tipo de quarto: Suíte
-        Quantidade de diárias: ${quantidadeDias}
-        Valor total da estadia: R$ ${calculoSuite.toFixed(2)}`);
-    }
+    let tipoQuarto = readlineSync.question("Qual tipo de quarto? ")
+    let quantidadeDias = readlineSync.questionInt("Quantas diárias? ")
+
+   
+     function calculo2(a, quantidadeDias) {
+         return a * quantidadeDias;
+     }
+
+    if (tipoQuarto == 'standard') {
+        console.log(`Tipo de quarto: Standard
+          Quantidade de diárias: ${quantidadeDias}
+          Valor total da estadia: R$ ${calculo2(150, quantidadeDias)}`);
+    } else if (tipoQuarto == 'luxo') {
+        console.log(`Tipo de quarto: Luxo
+          Quantidade de diárias: ${quantidadeDias}
+          Valor total da estadia: R$ ${calculo2(300, quantidadeDias)}`);
+    } else if (tipoQuarto == 'suite') {
+        console.log(`Tipo de quarto: Suíte
+          Quantidade de diárias: ${quantidadeDias}
+          Valor total da estadia: R$ ${calculo2(500, quantidadeDias)}`);
+    } else{
+         console.warn('Opção inválida, tente novamente!');
+         inicioDoPrograma()
+     }
+
+}
+inicioDoPrograma() 
